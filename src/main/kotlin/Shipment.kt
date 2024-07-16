cbf88537daeb7a522e18bb04a9d3c1af186d01b2
kmerk <mcmillenka@gmail.com>
2024-07-16 10:26:59 -0600

    Began working on shipment clas, updated UML

--- src/main/kotlin/Shipment.kt
+++ src/main/kotlin/Shipment.kt
@@ -1,6 +1,14 @@
 class Shipment : Observable {
+    val status: String = ""
+    val id : String = ""
+    val notes: MutableList<String> = mutableListOf()
+    val updateHistory: MutableList<String> = mutableListOf()
+    val expectedDeliveryDateTimestamp: Long? = null
+    val currentLocation: String = ""
+    private val observers: MutableList<Observer> = mutableListOf()
+
     override fun addObserver(observer: Observer) {
-        TODO("Not yet implemented")
+        observers.add(observer)
     }
 
     override fun removeObserver(observer: Observer) {
