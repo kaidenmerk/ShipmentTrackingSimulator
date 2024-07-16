675cce766dca83118db62ec3541c1575209fe136
kmerk <mcmillenka@gmail.com>
2024-07-16 10:38:51 -0600

    updated uml, implemented Shipment class, added abstract fun to ShippingUpdate

--- src/main/kotlin/Shipment.kt
+++ src/main/kotlin/Shipment.kt
@@ -1,8 +1,8 @@
-class Shipment : Observable {
+class Shipment(id: String) : Observable {
     val status: String = ""
-    val id : String = ""
+    val id: String = id
     val notes: MutableList<String> = mutableListOf()
-    val updateHistory: MutableList<String> = mutableListOf()
+    val updateHistory: MutableList<ShippingUpdate> = mutableListOf()
     val expectedDeliveryDateTimestamp: Long? = null
     val currentLocation: String = ""
     private val observers: MutableList<Observer> = mutableListOf()
@@ -12,10 +12,22 @@ class Shipment : Observable {
     }
 
     override fun removeObserver(observer: Observer) {
-        TODO("Not yet implemented")
+        observers.remove(observer)
     }
 
     override fun notifyObservers() {
-        TODO("Not yet implemented")
+        observers.forEach {
+            it.update(this)
+        }
+    }
+
+    // Applying update applies to status, updateHistory, expectedDeliveryDateTimestamp, and currentLocation
+    fun addUpdate(update: ShippingUpdate) {
+        updateHistory.add(update)
+        update.applyUpdate(this)
+    }
+
+    fun addNote (note: String) {
+        notes.add(note)
     }
 }
--- src/main/kotlin/ShippingUpdate.kt
+++ src/main/kotlin/ShippingUpdate.kt
@@ -1,2 +1,4 @@
 abstract class ShippingUpdate {
+
+    abstract fun applyUpdate(shipment: Shipment)
 }
