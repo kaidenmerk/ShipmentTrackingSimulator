a19a623e9ce3ccd104ec1fd0c1084fe543dd0e11
kmerk <mcmillenka@gmail.com>
2024-07-16 14:19:05 -0600

    added functions for trackerviewhelper and UI

--- src/main/kotlin/TrackerViewHelper.kt
+++ src/main/kotlin/TrackerViewHelper.kt
@@ -1,5 +1,23 @@
 class TrackerViewHelper : Observer {
+    private var shipmentId: String = ""
+    private var shipmentNotes: MutableList<String> = mutableListOf()
+    private var shipmentUpdateHistory: MutableList<String> = mutableListOf()
+    private var expectedShipmentDeliveryDate: Long? = null
+    private var shipmentStatus: String = ""
+    
     override fun update(shipment: Shipment) {
         TODO("Not yet implemented")
+    }
+
+    fun trackShipment(id: String) {
+        shipmentId = id
+    }
+
+    fun stopTracking() {
+        shipmentId = ""
+        shipmentNotes.clear()
+        shipmentUpdateHistory.clear()
+        expectedShipmentDeliveryDate = null
+        shipmentStatus = ""
     }
 }
--- src/main/kotlin/UserInterface.kt
+++ src/main/kotlin/UserInterface.kt
@@ -1,3 +1,25 @@
-class UserInterface {
-   
+class UserInterface(
+    private val simulator: TrackingSimulator,
+    private val viewHelper: TrackerViewHelper
+    ) {
+    fun trackShipment(id: String) {
+        val shipment = simulator.findShipment(id)
+        if (shipment != null) {
+            viewHelper.trackShipment(id)
+        } else {
+            displayError("Shipment with ID $id does not exist.")
+        }
+    }
+
+    fun stopTracking(id: String) {
+        viewHelper.stopTracking()
+    }
+
+    fun displayError(message: String) {
+        println("Error: $message")
+    }
+
+    fun updateDisplay() {
+        // Implement UI
+    }
 }
